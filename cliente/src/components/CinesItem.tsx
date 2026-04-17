f1325bc2e3e929dfd11279e7dd85c4fcd69406be
Javier Zamarrón Mateo <javier.zamarron23@estudiantes.uva.es>
2026-04-17 13:56:34 +0200

    Añadir botón de editar cine sin funcionalidad cuando eres Admin

--- cliente/src/components/CinesItem.tsx
+++ cliente/src/components/CinesItem.tsx
@@ -1,11 +1,25 @@
 import type { Cinema } from '../types/cinema'
+import { useAuth } from '../context/AuthContext';
 
 function CineItem({ cinema }: { cinema: Cinema }) {
-  return (
-    <div style={{ backgroundColor: '#1976d2', color: 'white', padding: '10px', marginBottom: '5px', borderRadius: '4px' }}>
-      {cinema.nombre}
-    </div>
-  )
-}
+  const { user } = useAuth();
+  const isAdmin = user?.role === 'ADMIN';
+
+return (
+    <>
+      <div style={{ backgroundColor: '#1976d2', color: 'white', padding: '10px', marginBottom: '5px', borderRadius: '4px' }}>
+        {cinema.nombre}
+      </div>
+      <div>
+        {isAdmin && (
+          <button 
+            style={{ backgroundColor: 'orange', color: 'white', border: 'none' }}>
+            Editar Cine 
+          </button>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default CineItem
